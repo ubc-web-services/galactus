80090006adfbcec7344dbad9e92a2eab2cfa028b
James <james.otoole@ubc.ca>
2016-06-03 23:47:29 -0700

    Update color module integration

--- color/preview.js
+++ color/preview.js
@@ -7,31 +7,33 @@
   "use strict";
 
   Drupal.color = {
-    logoChanged: false,
-    callback: function (context, settings, form, farb, height, width) {
+    callback: function (context, settings, $form) {
+      
+      var $colorPreview = $form.find('#preview');
+      var $colorPalette = $form.find('.js-color-palette');
       
       // Unit background.
-      form.find('#preview-ubc7-unit').css('background-color', form.find('.js-color-palette input[name="palette[unit]"]').val());
+      $colorPreview.find('#preview-ubc7-unit').css('background-color', $colorPalette.find('input[name="palette[unit]"]').val());
       
       // Text Colour
-      form.find('.all-content').css('color', form.find('.js-color-palette input[name="palette[text]"]').val());
+      $colorPreview.find('.all-content').css('color', $colorPalette.find('input[name="palette[text]"]').val());
       
       // Primary and Secondary Colours
-      form.find('.unit-content .primary').css('backgroundColor', form.find('.js-color-palette input[name="palette[primary]"]').val());
-      form.find('.unit-content .secondary').css('backgroundColor', form.find('.js-color-palette input[name="palette[secondary]"]').val());
+      $colorPreview.find('.unit-content .primary').css('backgroundColor', $colorPalette.find('input[name="palette[primary]"]').val());
+      $colorPreview.find('.unit-content .secondary').css('backgroundColor', $colorPalette.find('input[name="palette[secondary]"]').val());
       
       // Heading Colours
-      form.find('.unit-content h1').css('color', form.find('.js-color-palette input[name="palette[heading1]"]').val());
-      form.find('.unit-content h2').css('color', form.find('.js-color-palette input[name="palette[heading2]"]').val());
-      form.find('.unit-content h3').css('color', form.find('.js-color-palette input[name="palette[heading3]"]').val());
-      form.find('.unit-content h4').css('color', form.find('.js-color-palette input[name="palette[heading4]"]').val());
-      form.find('.unit-content h5').css('color', form.find('.js-color-palette input[name="palette[heading5]"]').val());
-      form.find('.unit-content h6').css('color', form.find('.js-color-palette input[name="palette[heading6]"]').val());
+      $colorPreview.find('.unit-content h1').css('color', $colorPalette.find('input[name="palette[heading1]"]').val());
+      $colorPreview.find('.unit-content h2').css('color', $colorPalette.find('input[name="palette[heading2]"]').val());
+      $colorPreview.find('.unit-content h3').css('color', $colorPalette.find('input[name="palette[heading3]"]').val());
+      $colorPreview.find('.unit-content h4').css('color', $colorPalette.find('input[name="palette[heading4]"]').val());
+      $colorPreview.find('.unit-content h5').css('color', $colorPalette.find('input[name="palette[heading5]"]').val());
+      $colorPreview.find('.unit-content h6').css('color', $colorPalette.find('input[name="palette[heading6]"]').val());
       
       // Links preview.
-      form.find('.unit-content .link').css('color', form.find('.js-color-palette input[name="palette[link]"]').val());
-      form.find('.unit-content .link:hover').css('color', form.find('.js-color-palette input[name="palette[linkhover]"]').val());
-      form.find('.unit-content .link:active').css('color', form.find('.js-color-palette input[name="palette[linkactive]"]').val());
+      $colorPreview.find('.unit-content .link').css('color', $colorPalette.find('input[name="palette[link]"]').val());
+      $colorPreview.find('.unit-content .link:hover').css('color', $colorPalette.find('input[name="palette[linkhover]"]').val());
+      $colorPreview.find('.unit-content .link:active').css('color', $colorPalette.find('input[name="palette[linkactive]"]').val());
 
     }
   };

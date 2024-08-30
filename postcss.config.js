672185b65921be7fcca562f58754b3288656fd65
James O'Toole <james.otoole@ubc.ca>
2024-08-29 21:11:31 -0700

    Replicate sass support using postcss
    Adds postcss processing without changing sass syntax
    
    SASS changes:
    - comment styling
    - removed mixins
    - removed wrapper class from ckeditor5 styles (now handled with postcss)
    - moved all imports to top of file (required)
    - sass partials have been moved into the sass/partials/ dir to prevent postcss processing
    
    Adds support for:
    - autoprefixer
    - css minification (not convinced we need to do this, but here for discussion)
    - imports
    - nesting (not convinced we need to do this, but here for discussion)
    - prefixed styles for ckeditor (see postcss.config.js)
    - sass variables
    
    Output dir has been changed to dist/css for testing / review

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    require("postcss-import"),
+    require("postcss-simple-vars"),
+    require("postcss-nesting"),
+    require("postcss-prefix-selector")({
+      prefix: ".ck-content",
+      exclude: ["body", "html", ":root"],
+      includeFiles: "sass/ckeditor5.scss",
+    }),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [require("cssnano")] : []),
+  ],
+};
